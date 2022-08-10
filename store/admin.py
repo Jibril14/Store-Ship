19ce2bb733f2da9e249868514c4ff6604930e1e5
Abdullahi Abdulwasiu <47268671+Jibril14@users.noreply.github.com>
2022-08-10 17:10:12 +0100

    Populate Model

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Product, Review, Order, OrderedItem, ShippingAddress
 # Register your models here.
+
+
+admin.site.register(Product)
+admin.site.register(Review)
+admin.site.register(Order)
+admin.site.register(OrderedItem)
+admin.site.register(ShippingAddress)
